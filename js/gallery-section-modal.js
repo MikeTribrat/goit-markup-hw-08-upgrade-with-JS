377db55805d2707d200de6343b6e81170d6ccb28
MikeTribrat <mike.lapp.ua@gmail.com>
2022-07-28 16:46:35 +0300

    done
    filter menu @ gallery page

--- js/gallery-section-modal.js
+++ js/gallery-section-modal.js
@@ -1,16 +1,64 @@
-
-const webBtn = document.querySelector('[data-action="js-btn-web"]');
-const nodeList = document.querySelectorAll('.gallery-section__card');
-const array = Array.prototype.slice.call(nodeList);
+const ref = {
+  allBtn: document.querySelector('[data-action="js-btn-all"]'),
+  webBtn: document.querySelector('[data-action="js-btn-web"]'),
+  appBtn: document.querySelector('[data-action="js-btn-app"]'),
+  designBtn: document.querySelector('[data-action="js-btn-design"]'),
+  marketingBtn: document.querySelector('[data-action="js-btn-marketing"]'),
+  webBtn: document.querySelector('[data-action="js-btn-web"]'),
+}
+  
+  const nodeList = document.querySelectorAll('.gallery-section__card');
+  const array = Array.prototype.slice.call(nodeList);
+  const toHide = []
 //console.log(array)
-const toHide = []
 
-webBtn.addEventListener('click', () => {
+
+ref.webBtn.addEventListener('click', () => {
   for (i = 0; i <= array.length - 1; i++){
-    if (array[i].id ==='js-web') {
+    array[i].classList.remove('visually-hidden')
+    if (array[i].id !== 'js-web') {
       toHide.push(array[i])
-      array[i].classList.toggle('visually-hidden')
-      //console.log(array[i].id)
+      array[i].classList.add('visually-hidden')
+      console.log(toHide)
+    }
+}
+})
+
+ref.allBtn.addEventListener('click', () => {
+  for (i = 0; i <= array.length - 1; i++)
+      array[i].classList.remove('visually-hidden')
+      console.log(toHide)
+    }
+)
+
+ref.appBtn.addEventListener('click', () => {
+  for (i = 0; i <= array.length - 1; i++){
+    array[i].classList.remove('visually-hidden')
+    if (array[i].id !== 'js-app') { 
+      toHide.push(array[i])
+      array[i].classList.add('visually-hidden')
+      console.log(toHide)
+    }
+}
+})
+
+ref.designBtn.addEventListener('click', () => {
+  for (i = 0; i <= array.length - 1; i++){
+    array[i].classList.remove('visually-hidden')
+    if (array[i].id !== 'js-design') { 
+      toHide.push(array[i])
+      array[i].classList.add('visually-hidden')
+      console.log(toHide)
+    }
+}
+})
+
+ref.marketingBtn.addEventListener('click', () => {
+  for (i = 0; i <= array.length - 1; i++){
+    array[i].classList.remove('visually-hidden')
+    if (array[i].id !== 'js-marketing') { 
+      toHide.push(array[i])
+      array[i].classList.add('visually-hidden')
       console.log(toHide)
     }
 }
